4dfab38ae4b4d5962b97f144eceef7f44bd10ace
huber <emmanuel.bitsindou@etudiant.univ-reims.fr>
2025-09-29 20:09:50 +0200

    feat:  Mise en place de l'affichage

--- TP1/src/App.jsx
+++ TP1/src/App.jsx
@@ -1,6 +1,13 @@
+import React, {use} from "react";
+import Input from "./components/forms/Input.jsx";
+import Checkbox from "./components/forms/Checkbox.jsx";
+import UserRow from "./components/users/UserRow.jsx";
+import UserRoleRow from "./components/users/UserRoleRow.jsx";
+
 const USERS = [
     { role: "Admin", name: "Alice", age: 30, active: true, email: "alice@example.com" },
     { role: "Admin", name: "Bob", age: 45, active: false, email: "bob@example.com" },
+    { role: "Admin", name: "Tina", age: 39, active: true, email: "tina@example.com" },
     { role: "User", name: "Charlie", age: 22, active: true, email: "charlie@example.com" },
     { role: "User", name: "Diana", age: 28, active: false, email: "diana@example.com" },
     { role: "Moderator", name: "Eve", age: 35, active: true, email: "eve@example.com" },
@@ -18,14 +25,64 @@ const USERS = [
     { role: "Moderator", name: "Quinn", age: 37, active: false, email: "quinn@example.com" },
     { role: "User", name: "Rachel", age: 32, active: true, email: "rachel@example.com" },
     { role: "Guest", name: "Steve", age: 21, active: false, email: "steve@example.com" },
-    { role: "Admin", name: "Tina", age: 39, active: true, email: "tina@example.com" }
 ];
 
 
 function App () {
-    return <div>
+    return (<div className="container">
         <h1>TP1 with React</h1>
-    </div>
+        <SearchBar/>
+        <UserTable users={USERS}/>
+    </div>);
+}
+
+function SearchBar () {
+    return (
+        <div>
+            <div>
+                <Input
+                    id="search"
+                    placeholder="Rechercher..."
+                    onChange={() => null}
+                    value=""
+                />
+                <Checkbox id="checked" label="N'affiche que les personnes disponible" onChanges={() => null} checked={true}/>
+            </div>
+        </div>
+    );
+}
+
+function UserTable ({users}) {
+
+    const rows = []
+    let lastRole = null
+
+    const sortedUsers = [...users].sort((a, b) => a.role.localeCompare(b.role))
+
+    for(let user of sortedUsers) {
+        if(user.role !== lastRole) {
+            rows.push(<UserRoleRow key={user.role} name={user.role}/>)
+        }
+        lastRole = user.role
+        rows.push(<UserRow user={user} key={user.name}/>)
+
+    }
+
+    return (
+        <table className="table">
+            <caption>La listes des personnes du Magasins</caption>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Age</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+    );
 }
 
 export default App
